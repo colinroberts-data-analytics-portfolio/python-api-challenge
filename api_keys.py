8dc189b166b4664dda0cc0b7823642ca6972ccfd
Colin Roberts <156542748+colinroberts-data-analytics-portfolio@users.noreply.github.com>
2024-04-08 22:35:33 -0500

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "275889680afc8a620cc08fbfdecc7161"
+
+# Geoapify API Key  or 09aa7cbe22a94cc6b97c3da44f07a3e1  or 3b1a5f21c5d743d488573bafcc6da9fe
+geoapify_key = "3b1a5f21c5d743d488573bafcc6da9fe"
